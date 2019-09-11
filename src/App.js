c43d09e32fa1b876718d3620422565b7ff78223d
Elias Johansson <elias_johansson@hotmail.se>
2019-09-11 12:31:15 +0200

    Added auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import awsconfig from './aws-exports';
 
+Amplify.configure(awsconfig);
+const loggedIn = true;
 function App() {
   return (
     <div>
       <h1>App</h1>
+
+      {loggedIn ? <h1>Logged in</h1> : <h1>Not logged in</h1>}
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, true);
