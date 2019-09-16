5295d0e23f53373c4e8dedeafd5d016f8d165367
Elias Johansson <elias_johansson@hotmail.se>
2019-09-16 12:54:17 +0200

    Added plus icon and playlist mock data

--- src/pages/MyPlaylists/index.js
+++ src/pages/MyPlaylists/index.js
@@ -3,11 +3,51 @@ import Banner from '../../components/Banner';
 import { Heading } from '../../components/Typography';
 import { Grid } from '../../components/Util';
 import ListCard from '../../components/ListCard';
+import Plus from '../../components/Icons/Plus';
+
+const playlists = [
+  {
+    title: 'Ljus',
+    image:
+      'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    title: 'Scenografi',
+    image:
+      'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    title: 'Teater',
+    image:
+      'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    title: 'Dans',
+    image:
+      'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    title: 'Opera',
+    image:
+      'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    title: 'Ljud',
+    image:
+      'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    title: 'Konst',
+    image:
+      'https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  }
+];
 
 const MyPlaylists = () => {
   return (
     <div>
       <Banner
+        tint="rgba(0,0,0,.85)"
         image="https://images.unsplash.com/photo-1568621779193-e6e6c9ab80f0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80"
         justifyContent="space-between"
         px="3"
@@ -15,24 +55,12 @@ const MyPlaylists = () => {
         <Heading m="0" color="white" as="h3">
           Mina Listor
         </Heading>
+        <Plus />
       </Banner>
       <Grid p="3" gridTemplateColumns="1fr 1fr" gridGap="2">
-        <ListCard
-          title="Ljus"
-          image="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-        />
-        <ListCard
-          title="Scenografi"
-          image="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-        />
-        <ListCard
-          title="Teater"
-          image="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-        />
-        <ListCard
-          title="Dans"
-          image="https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-        />
+        {playlists.map(p => (
+          <ListCard key={p.title} title={p.title} image={p.image} />
+        ))}
       </Grid>
     </div>
   );
